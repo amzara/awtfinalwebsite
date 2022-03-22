47cc79e987322198997031589690430fbf7ad370
amzara <99461764+amzara@users.noreply.github.com>
2022-03-22 10:40:26 +0800

    Update scienceAssessment1.php
    10.40update

--- scienceAssessment1.php
+++ scienceAssessment1.php
@@ -7,7 +7,8 @@ $user_data = check_login($conn);
 
 <h1>Instructions</h1>
 <p>This is a multiple choice question quiz. Select the correct answer and submit it. 
-Extra features : Show correct answer for every question + marks after assessment </p>
+Extra features : Show correct answer for every question + marks after assessment + hide submit button after user complete(so cannot do multiple times)maybe use jquery on button to just make it disappear. </p>
+
 
 <form method="POST">
 Question 1: Why are bacteria's classified as microorganisms?<br>
@@ -18,18 +19,43 @@ Question 1: Why are bacteria's classified as microorganisms?<br>
 Question 2: You are:<br>
 <select name="sa1q2">
     <option value="a">Gay.</option>
-    <option value="b">Ill.</option>
+    <option value="b">Straight.</option>
 </select><br>
 <input type="submit">
 </form>
 
 <?php
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+$marks=0;
 $sa1q1=$_POST["sa1q1"];
 $sa1q2=$_POST["sa1q2"];
 
 echo $sa1q1;
 echo $sa1q2;
 
+if($sa1q1=="b"){
+$marks+=50;
+}
+else{
+echo "The correct answer for question 1 is They are super small and can only be seen under a microscope.<br>";
+}
+
+if($sa1q2=="b"){
+    $marks+=50;
+}
+    else{
+    echo "The correct answer for question 2 is Straight";
+    }
+
+echo "Your total score is " . $marks . "% !";
+
+echo "<a href='homepage.php'>Click here to return to homepage</a>";
+
+
+
 
 }
+
+
+
+?>
